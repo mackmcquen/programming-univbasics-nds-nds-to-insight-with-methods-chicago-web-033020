5ea16789fe8134b89a9484188b89d63d6de1ec47
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 23:29:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,8 @@ require 'directors_database'
 def gross_for_director(director_data)
 movie_index = 0
 director_total = 0
-while movie_index < director_data[:movies]
+while movie_index < director_data[:movies].length
+
 end
 
 # Write a method that, given an NDS creates a new Hash
