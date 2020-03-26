2720ec1cff9e357a25b2d8f02202eaf9b7cc4940
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 23:29:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,9 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-gross
+movie_index = 0
+director_total = 0
+
 end
 
 # Write a method that, given an NDS creates a new Hash
